a3d2eb0a9e440559001b25b2bd9bd879ed350c6d
Anton <ant99norgren@gmail.com>
2024-11-08 11:56:24 +0100

    Worked on enemy projectiles and added more variables to projectiles

--- Assets/Scripts/projectileFriendlyScript.cs
+++ Assets/Scripts/projectileFriendlyScript.cs
@@ -7,18 +7,30 @@ public class projectileFriendlyScript : MonoBehaviour
 
     public float speed;
     public int damage;
+    public float knockback;
+    private Vector3 startPosition;
+    public float range;
     public int bounces;
+    public float rangeAfterBounce;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         this.transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        float distanceTraveled = Vector3.Distance(startPosition, transform.position);
+
+        // If the distance exceeds the target, destroy the object
+        if (distanceTraveled >= range)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
--- Assets/Scripts/projectileHostileScript.cs
+++ Assets/Scripts/projectileHostileScript.cs
@@ -5,19 +5,47 @@ using UnityEngine;
 public class projectileScript : MonoBehaviour
 {
 
-    public int damage;
     public float speed;
-    public int bounce;
+    public int damage;
+    public float knockback;
+    private Vector3 startPosition;
+    public float range;
+    public int bounces;
+    public float rangeAfterBounce;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        float distanceTraveled = Vector3.Distance(startPosition, transform.position);
+
+        // If the distance exceeds the target, destroy the object
+        if (distanceTraveled >= range)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the object hit is the player
+        if (other.CompareTag("Player"))
+        {
+            healthManager healthManager = other.GetComponent<healthManager>();
+            if (healthManager != null)
+            {
+                healthManager.takeDamage(damage);
+            }
+
+            // Destroy the projectile
+            Destroy(gameObject);
+        }
     }
 }
