ce9a658abbb963be6de2ed5b3ba48bf8dadaabb9
unknown <ht22erst@axevalla.fhsk.se>
2024-11-15 11:54:44 +0100

    Help

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -8,23 +8,36 @@ public class EnemyScript : MonoBehaviour
     //public GameObject player;
     public Transform Player;
     int MoveSpeed = 4;
-    int MaxDist = 10;
-    int MinDist = 5;
-    // Start is called before the first frame update
+    //int MaxDist = 10;
+    //int MinDist = 5;
+    bool Canshoot = true;
+    
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         float dist = Vector3.Distance(Player.position, transform.position);
-        if ()
+        if (dist > 5)
         {
             var step = MoveSpeed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, Player.position, step);
         }
-
+    }
+    IEnumerator pang() 
+    {
+        while (true) 
+        { 
+            if (Canshoot == true)
+            {
+                //Shoot mechanics here
+                //Canshoot = false
+            }
+            yield return new WaitForSeconds(1);
+            //Canshoot = true
+            yield return null;
+        }
     }
 }
